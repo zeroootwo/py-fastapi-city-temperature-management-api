f72c6d4d101097448d158dc9643ef9eafcaf2241
Anastasiia Velikaya <a.v.zaychenko@gmail.com>
2026-03-15 22:45:25 +0200

    Solution_3

--- crud.py
+++ crud.py
@@ -9,7 +9,7 @@ def get_city(db: Session, city_id: int):
     return db.query(models.City).filter(models.City.id == city_id).first()
 
 def create_city(db: Session, city: schemas.CityCreate):
-    db_city = models.City(name=city.name, additional_info=city.additional_info)
+    db_city = models.City(**city.model_dump())
     db.add(db_city)
     db.commit()
     db.refresh(db_city)
